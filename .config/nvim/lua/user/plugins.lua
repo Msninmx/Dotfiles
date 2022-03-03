8eb40144d855a7dbb8c268c1a193ec75793b5405
Miguel Nguyen <msninmx@gmail.com>
2022-03-02 22:09:16 -0600

    updated plugins

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -133,6 +133,7 @@ return packer.startup(function(use)
       }
     end
   }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
